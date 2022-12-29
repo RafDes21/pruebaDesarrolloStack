4c98658629260c876412dba93c7a208aea6ef835
Israel <israeltineo84@gmail.com>
2022-12-29 20:03:27 -0300

    config env

--- backend/src/routes/clientes.routes.ts
+++ backend/src/routes/clientes.routes.ts
@@ -4,15 +4,15 @@ import { Clientes } from "../models/clientes";
 const route = Router();
 
 route.get("/clientes", async (_req, res) => {
-  await Clientes.find().then((data) => res.json(data));
-  res.status(200);
+  const data = await Clientes.find();
+  res.json(data);
 });
 
 route.get("/clientes/:id", async (req, res) => {
   const { id } = req.params;
 
-  await Clientes.findById(id).then((data) => res.json(data));
-  res.status(200);
+  const data = await Clientes.findById(id);
+  res.json(data);
 });
 
 route.post("/clientes", async (req, res) => {
@@ -30,17 +30,16 @@ route.put("/clientes/:id", async (req, res) => {
     { _id: id },
     { $set: { nombre, documento, direccion, telefono } }
   );
-  res.send("cliente modificado");
+  res.status(200);
 });
 
 route.delete("/clientes", async (req, res) => {
   const ids: string = req.query.ids as string;
 
   if (ids) {
-    await Clientes.deleteMany({ _id: { $in: ids.split(",") } }).then((data) =>
-      res.json(data)
-    );
+    await Clientes.deleteMany({ _id: { $in: ids.split(",") } });
   }
+  res.status(200);
 });
 
 export default route;
