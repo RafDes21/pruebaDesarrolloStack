fd6d7986eb9751e85838dbb28ae0aeae3297f238
Israel <israeltineo84@gmail.com>
2022-12-29 19:19:48 -0300

    gitignore

--- backend/build/routes/clients.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const route = (0, express_1.Router)();
-route.get("/", (_req, res) => {
-    res.send("clientes");
-});
-exports.default = route;
-//# sourceMappingURL=clients.routes.js.map
